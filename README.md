8fda794f93d79b610b8763f80ba19eafc9089e51
Jase Pellerin <jasepellerin@gmail.com>
2017-08-22 08:35:01 -0600

    Changed lint options, added readme, and updated index

---
+++ README.md
@@ -0,0 +1,36 @@
+# vue-scroll-class #
+
+A [Vue.js](https://vuejs.org/) directive to add a class to an based on vertical scroll.
+
+## Installation ##
+
+`npm i -S vue-scroll-class`
+
+## Import ##
+
+`import VueScrollClass from 'vue-scroll-class';`
+
+## Add to Vue ##
+
+Register to Vue globally:
+
+`Vue.directives('scroll-class': VueScrollClass)`
+
+Register to a specific Vue instance:
+
+`const app = new Vue({
+  ...
+  directives: {
+    'scroll-class': VueScrollClass,
+  },
+  ...
+});`
+
+## Usage ##
+
+vue-scroll-class defaults to adding the class 'sticky' to the attached element after the page has been scrolled 100px.
+`<div v-scroll-class>I'll get the class 'sticky' after 100px!</div>`
+
+To override these defaults, add a class name or a different number of pixels in the appropriate place:
+
+`<div v-scroll-class:differentClass=645>I'll get the class 'differentClass' after 645px!>/div>`
--- source/index.js
+++ source/index.js
@@ -1,13 +1,26 @@
 /* eslint no-undef: "off" */
+let classAdded = false;
+
+/* Handles page scrolling and applies class as necessary
+ * @param {Element} el - The element attached to the directive
+ * @param {num} scrollHeight - Apply class to element when page is
+      scrolled this many pixels from top
+ * @param {string} classToAdd - Class to add and remove
+ */
 const handleScroll = (el, scrollHeight = 100,
-  addedClass = 'sticky') => {
-  if (window.scrollY >= scrollHeight) {
-    el.classList.add(addedClass);
-  } else {
-    el.classList.remove(addedClass);
+  classToAdd = 'sticky') => {
+  if (window.scrollY >= scrollHeight && !classAdded) {
+    el.classList.add(classToAdd);
+    classAdded = true;
+  } else if (window.scrollY < scrollHeight && classAdded) {
+    el.classList.remove(classToAdd);
+    classAdded = false;
   }
 };
 
+/* Adds event listener to the window based on scroll
+ * When page is scrolled, call handlescroll with given arguments
+ */
 const VueScrollClass = {
   bind: (el, binding) => {
     window.addEventListener('scroll', () => {
