46ff171fec6046eeaa61fa8f065e00ae3c842091
Jase Pellerin <jasepellerin@gmail.com>
2017-08-23 09:46:32 -0600

    Added mocha reporter and fixed env

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -38,7 +38,7 @@ module.exports = (config) => {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
 
     // web server port
