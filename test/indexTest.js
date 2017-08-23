7bf21dde1e0a3169f6d270959b21ede6643fab20
Jase Pellerin <jasepellerin@gmail.com>
2017-08-23 11:16:48 -0600

    Got dom injection working for tests. Need to get height working for scroll

--- test/indexTest.js
+++ test/indexTest.js
@@ -9,29 +9,52 @@ describe('vue-scroll-class', () => {
   //  const testScrollDistance = 500
 
   describe('defaults', () => {
-    let vm
+    let app
+    let oldApp
 
     beforeEach(() => {
-      vm = new Vue({
-        template: `<div style="{height: 1000px}">
-                  <span v-scroll-class>hello</span></div>`,
+      // Create a basic div with id app and append to document
+      app = document.createElement('div')
+      app.setAttribute('id', 'app')
+      document.body.appendChild(app)
+
+      // Mount Vue instance to #app
+      new Vue({
         directives: {
           'scroll-class': Vsc
         }
-      }).$mount()
+      }).$mount('#app')
+
+      // Set body height to allow scrolling
+      document.body.style.height = '10000px'
+    })
+
+    afterEach(() => {
+      // Remove old #app divs
+      oldApp = document.getElementById('app')
+      if (oldApp) {
+        document.body.removeChild(oldApp)
+      }
     })
 
     it('should not apply class before scrolling', () => {
       expect(window.pageYOffset).to.be(0)
-      expect(vm.$el.firstChild.classList.contains(defaultClass)).to.be(false)
+      expect(document.getElementById('app')
+        .classList.contains(defaultClass)).to.be(false)
     })
 
-    it('should apply class after scrolling default distance', () => {
-      console.log(window.innerHeight)
+    it('should apply class after scrolling default distance', (done) => {
+      console.log('Body height: ' + document.body.style.height)
+      console.log('Window height: ' + window.innerHeight)
       expect(window.pageYOffset).to.be(0)
       window.scrollTo(0, 100)
-      expect(window.pageYOffset).to.be(100)
-      expect(vm.$el.firstChild.classList.contains(defaultClass)).to.be(true)
+      window.setTimeout(() => {
+        console.log(window.pageYOffset)
+        expect(window.pageYOffset).to.be(100)
+        expect(document.getElementById('app')
+          .classList.contains(defaultClass)).to.be(true)
+        done()
+      }, 1000)
     })
   })
 })
